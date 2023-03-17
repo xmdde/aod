fbcc8258306db7f73fbed399344db8075fcc9d52
justynaziemichod <268431@student.pwr.edu.pl>
2023-03-17 12:50:48 +0100

    init

---
+++ graphs/2.h
@@ -0,0 +1,4 @@
+#ifndef LAB1_2_H
+#define LAB1_2_H
+
+#endif //LAB1_2_H
--- graphs/main.cpp
+++ graphs/main.cpp
@@ -12,7 +12,7 @@ private:
     vector<vector<int>> adj;
 
 public:
-    Graph(int n) { //mamy n+1 elementow, numerujemy wierzcholki 1...n
+    Graph(int n) { //numerujemy wierzcholki 1...n
         numOfNodes = n;
         adj = *new vector<vector<int>>;
         for (int i = 0; i <= numOfNodes; i++) {
@@ -24,18 +24,6 @@ public:
         adj[a].push_back(b);
     }
 
-    void showDegrees() {
-        for(auto x: adj) {
-            cout << "outdegree: " << x.size() << '\n';
-        }
-    }
-
-    void showvisits() {
-        for (int i = 0; i < numOfNodes; i++) {
-            //cout  << "visited: " << visited[i] << '\n';
-        }
-    }
-
     void DFS(int start) {
         vector <bool> visited(numOfNodes + 1, false);
         cout << "DFS: ";
@@ -44,7 +32,7 @@ public:
     }
 
     void dfsUtil(int start, vector<bool>& visited) {
-        visited[start] = true; //uwazaj na to
+        visited[start] = true;
         cout << start << ' ';
         for (int i = 0; i < adj[start].size(); i++) {
             if (!visited[adj[start][i]])
@@ -52,8 +40,9 @@ public:
         }
     }
 
-    void BFS(int start) {
+    void BFS(int start, bool print) {
         vector <bool> visited(numOfNodes + 1, false);
+        Graph bfsTree = *new Graph(numOfNodes);
         cout << "BFS: ";
         queue<int> q;
         visited[start] = true;
@@ -68,34 +57,54 @@ public:
                 if (!visited[i]) {
                     visited[i] = true;
                     q.push(i);
+                    bfsTree.addEdge(current, i);
+                    bfsTree.addEdge(i, current);
                 }
             }
+        }
+        cout << '\n';
+        if (print) {
+            bfsTree.printGraph();
+        }
+    }
+
+    void printGraph() {
+        for (int i = 1; i <= numOfNodes; i++) {
+            cout << i << ": ";
+            for (auto a : adj[i]) {
+                cout << a << ' ';
+            }
+            cout << '\n';
         }
     }
 
     ~Graph() {
-        //cout << "destructor executed \n";
+        //add proper destructor
     }
 };
 
 int main() {
-    ifstream ifs = ifstream("/Users/justynaziemichod/Documents/SEM4/aod/lab1/data.txt", ios_base::in);
+    ifstream ifs = ifstream("/Users/justynaziemichod/Documents/SEM4/aod/lab1/data/data.txt", ios_base::in);
 
     if (ifs.is_open()) {
+        char type;
         int a;
         int b;
         int m;
+        ifs >> type;
         ifs >> a >> m;
         Graph g = *new Graph(a);
 
         while (m > 0) {
             ifs >> a >> b;
             g.addEdge(a, b);
+            if (type == 'U') {
+                g.addEdge(b, a);
+            }
             m--;
         }
-
+        
         ifs.close();
-        g.DFS(2);
-        g.BFS(2);
+        g.BFS(1, true);
     }
 }
