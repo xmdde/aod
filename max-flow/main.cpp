97cb78f273a8d9420046c3f03a03a189f4e5d218
justynaziemichod <268431@student.pwr.edu.pl>
2023-06-21 22:29:09 +0200

    hypercube maximum flow done

--- max-flow/main.cpp
+++ max-flow/main.cpp
@@ -1,6 +1,26 @@
 #include <iostream>
+#include <fstream>
 #include <chrono>
 #include "Hypercube.h"
+void glpkModel(Hypercube* hypercube, const int k);
+
+void stats(const int k, const int meta) {
+    int d = 20;
+    long long time = 0;
+    long long flow = 0;
+    long long paths = 0;
+    for (int i = 0; i < d; i++) {
+        auto begin = std::chrono::high_resolution_clock::now();
+        Hypercube tmp = Hypercube(k);
+        int p = -1;
+        flow += tmp.maxFlow(0, meta, p);
+        paths += p;
+        auto t = std::chrono::duration_cast<std::chrono::microseconds>( std::chrono::high_resolution_clock::now() - begin);
+        time += t.count();
+    }
+    std::cout << "flow, time, paths:\n";
+    std::cout << flow/d << ' ' << time/d << ' ' << paths/d;
+}
 
 int main(int argc, char** argv) {
     int k;
@@ -19,11 +39,28 @@ int main(int argc, char** argv) {
 
     Hypercube hypercube = Hypercube(k);
     std::cout << "max flow: " << hypercube.maxFlow(0, meta, numOfPaths) << '\n';
-    auto t = std::chrono::duration_cast<std::chrono::milliseconds>( std::chrono::high_resolution_clock::now() - begin);
-    if (argc == 4 && std::string(argv[3]) == "-printFlow")
+    auto t = std::chrono::duration_cast<std::chrono::microseconds>( std::chrono::high_resolution_clock::now() - begin);
+
+    if ((argc >= 4 && std::string(argv[3]) == "-printFlow") || (argc == 6 && std::string(argv[5]) == "-printFlow"))
         hypercube.printFlow();
 
-    std::cerr << "t = " << t.count() << " ms\n";
+    std::cerr << "t = " << t.count() << " µs\n";
     std::cerr << "number of paths found: " << numOfPaths;
+
+    if ((argc >= 4 && std::string(argv[3]) == "-glpk") || (argc == 6 && std::string(argv[5]) == "-glpk") || (argc >= 5 && std::string(argv[4]) == "-glpk"))
+        glpkModel(&hypercube, k);
     return 0;
+}
+
+void glpkModel(Hypercube* hypercube, const int k) {
+    std::ofstream file("/Users/justynaziemichod/Documents/SEM4/aod/models/model1.txt", std::ios_base::out);
+    file << hypercube->v << '\n';
+    file << pow(2, k-1) * k << '\n';
+    for (int it = 0; it < hypercube->v; it++) {
+        for (auto e : hypercube->adj[it]) {
+            file << e->s << ' ' << e->t << ' ' << e->capacity << '\n';
+        }
+    }
+    file << "0\n" << hypercube->v-1;
+    file.close();
 }
